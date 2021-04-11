b46cbb058cdd326a4508dcb4ab3d5e37815e17b9
oreuis <77153448+oreuis@users.noreply.github.com>
2021-04-11 20:09:54 +0200

    Add files via upload

---
+++ Adapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace wzorzec6
+{
+    public interface Czytaj_tekst
+    {
+        string Czytaj();
+    }
+
+    class czyt
+    {
+        public string czyta_tekst()
+        {
+            return "da się przeczytać";
+        }
+    }
+
+    class Adapter : Czytaj_tekst
+    {
+        private readonly czyt tlumacz;
+
+        public Adapter(czyt tlumacz_tekst)
+        {
+            this.tlumacz = tlumacz_tekst;
+        }
+
+        public string Czytaj()
+        {
+            return $"To, co było w innym języku '{this.tlumacz.czyta_tekst()}'";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            czyt tekst_w_jezyku = new czyt();
+            Czytaj_tekst target = new Adapter(tekst_w_jezyku);
+
+            Console.WriteLine(target.Czytaj());
+        }
+    }
+}
